e1ef54078a2d99a39cfa3d2c1ab46e6a8d81c493
Kieran Gallagher <kags1995@hotmail.co.uk>
2019-09-23 18:01:49 +0100

    Lab 1

---
+++ Lab1/pch.h
@@ -0,0 +1,13 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <iostream>
+#include <string>
+#include <chrono>
+
+#include <GLEW/GL/glew.h>
+
+#include <SDL/SDL.h>
+#include <SDL/SDL_opengl.h>
+
+#endif //PCH_H

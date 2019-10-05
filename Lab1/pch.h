cf165f2d8b8dd9522ae42155e2e50ee4449e213f
Kieran Gallagher <kags1995@hotmail.co.uk>
2019-10-05 09:13:10 +0100

    Lab 2 complete

--- Lab1/pch.h
+++ Lab1/pch.h
@@ -4,10 +4,20 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <vector>
+#include <stdio.h>
+#include <fstream>
 
 #include <GLEW/GL/glew.h>
 
 #include <SDL/SDL.h>
 #include <SDL/SDL_opengl.h>
+
+#include <glm/glm.hpp>
+#include <glm/common.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <glm/gtx/string_cast.hpp>
 
 #endif //PCH_H
